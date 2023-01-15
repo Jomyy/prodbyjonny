de8fd1befe3c18824137f7d351a293e332ecf20f
jonny <joniamhandy@gmail.com>
2023-01-15 12:11:24 +0100

    loadingbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,26 @@
 <script>
     import { page } from '$app/stores';
+    import NProgress from 'nprogress';
+	import { navigating } from '$app/stores';
+ 
+	// NProgress css
+	import 'nprogress/nprogress.css';
+ 
+	NProgress.configure({
+		// Full list: https://github.com/rstacruz/nprogress#configuration
+		minimum: 0.16,
+        showSpinner:false
+
+	});
+ 
+	$: {
+		if ($navigating) {
+			NProgress.start();
+		}
+		if (!$navigating) {
+			NProgress.done();
+		}
+	}
 </script>
 <nav>
     <div>
