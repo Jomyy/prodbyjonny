33b3c8a17596d3557862f558744e052afe61574f
jonny <joniamhandy@gmail.com>
2023-02-06 13:49:51 +0100

    adg

--- src/routes/release/[name]/+page.svelte
+++ src/routes/release/[name]/+page.svelte
@@ -19,11 +19,6 @@
 	}
 </script>
 
-<svelte:head>
-	<meta property="og-title" content={data.post.releaseData.name} />
-	<meta property="og:image" content={data.post.imageUrl} />
-	<title>{data.post.releaseData.name}</title>
-</svelte:head>
 <MetaTags
 	openGraph={{
 		type: 'website',
